308ea1e67358c431dae8ebfff298dfab08706db1
Kamil Jakubisiak <u87_kamjak_waw@technischools.com>
2023-10-15 10:30:49 +0200

    comit 2,5

--- script.js
+++ script.js
@@ -1,16 +1,36 @@
 const aside = document.querySelector("aside");
 const openButton = document.querySelector("#open");
-const usersPanel = document.querySelector(".users");
 
 openButton.addEventListener("click", () => {
   aside.classList.toggle("aside-open");
   aside.classList.remove("aside-closed");
-  usersPanel.classList.remove("users-visible");
+  setInterval(displayFlexSet, 5);
 });
 
 const closeButton = document.querySelector("#close");
+
 closeButton.addEventListener("click", () => {
   aside.classList.toggle("aside-closed");
   aside.classList.remove("aside-open");
-  usersPanel.classList.toggle("users-visible");
+  setInterval(displayNoneSet, 511);
 });
+
+function displayNoneSet() {
+  aside.style.display = "none";
+}
+function displayFlexSet() {
+  aside.style.display = "flex";
+}
+
+const songsContainer = document.querySelector("#songs");
+
+async function songs() {
+  const json = await fetch(
+    "https://gist.githubusercontent.com/techniadrian/c39f844edbacee0439bfeb107227325b/raw/81eec7847b1b3dfa1c7031586405c93e9a9c1a2d/songs.json"
+  );
+  const response = await json.json();
+  response.forEach((item) => {
+    item = + songsContainer;
+  });
+}
+songs();
