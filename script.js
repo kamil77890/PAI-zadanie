d071a0e6229b1b1fb01d6497297fc8236ecadec4
Kamil Jakubisiak <u87_kamjak_waw@technischools.com>
2023-10-18 23:01:18 +0200

    comit 6

--- script.js
+++ script.js
@@ -2,29 +2,9 @@ const aside = document.querySelector("aside");
 const openButton = document.querySelector("#open");
 const closeButton = document.querySelector("#close");
 const songsContainer = document.querySelector("#songs");
-const leftAside = document.querySelector(".right-side");
+const rightAside = document.querySelector(".right-side");
 
 const songs = [];
-
-openButton.addEventListener("click", () => {
-  aside.classList.toggle("aside-open");
-  aside.classList.remove("aside-closed");
-  displayFlexSet();
-});
-
-closeButton.addEventListener("click", () => {
-  aside.classList.toggle("aside-closed");
-  aside.classList.remove("aside-open");
-  displayNoneSet();
-});
-
-function displayNoneSet() {
-  aside.style.display = "none";
-}
-
-function displayFlexSet() {
-  aside.style.display = "flex";
-}
 
 async function fetchAndDisplaySongs() {
   const response = await fetch(
@@ -89,9 +69,81 @@ function search_song() {
 }
 
 function infoFunction(item) {
-  leftAside.style.display = "block";
+  rightAside.style.display = "block";
   const titleElement = document.querySelector(".title");
   const backgroundElement = document.querySelector(".background");
   titleElement.querySelector("h1").innerHTML = item.title;
   backgroundElement.innerHTML = `<img class="background" src="${item.coverUrl}" alt="song" />`;
 }
+
+// losowy kod z internetu
+import lottieWeb from "https://cdn.skypack.dev/lottie-web";
+
+const playIconContainer = document.getElementById("play-icon");
+const audioPlayerContainer = document.getElementById("audio-player-container");
+const seekSlider = document.getElementById("seek-slider");
+const volumeSlider = document.getElementById("volume-slider");
+const muteIconContainer = document.getElementById("mute-icon");
+let playState = "play";
+let muteState = "unmute";
+
+const playAnimation = lottieWeb.loadAnimation({
+  container: playIconContainer,
+  path: "https://maxst.icons8.com/vue-static/landings/animated-icons/icons/pause/pause.json",
+  renderer: "svg",
+  loop: false,
+  autoplay: false,
+  name: "Play Animation",
+});
+
+const muteAnimation = lottieWeb.loadAnimation({
+  container: muteIconContainer,
+  path: "https://maxst.icons8.com/vue-static/landings/animated-icons/icons/mute/mute.json",
+  renderer: "svg",
+  loop: false,
+  autoplay: false,
+  name: "Mute Animation",
+});
+
+playAnimation.goToAndStop(14, true);
+
+playIconContainer.addEventListener("click", () => {
+  if (playState === "play") {
+    playAnimation.playSegments([14, 27], true);
+    playState = "pause";
+  } else {
+    playAnimation.playSegments([0, 14], true);
+    playState = "play";
+  }
+});
+
+muteIconContainer.addEventListener("click", () => {
+  if (muteState === "unmute") {
+    muteAnimation.playSegments([0, 15], true);
+    muteState = "mute";
+  } else {
+    muteAnimation.playSegments([15, 25], true);
+    muteState = "unmute";
+  }
+});
+
+const showRangeProgress = (rangeInput) => {
+  if (rangeInput === seekSlider) {
+    audioPlayerContainer.style.setProperty(
+      "--seek-before-width",
+      (rangeInput.value / rangeInput.max) * 100 + "%"
+    );
+  } else {
+    audioPlayerContainer.style.setProperty(
+      "--volume-before-width",
+      (rangeInput.value / rangeInput.max) * 100 + "%"
+    );
+  }
+};
+
+seekSlider.addEventListener("input", (e) => {
+  showRangeProgress(e.target);
+});
+volumeSlider.addEventListener("input", (e) => {
+  showRangeProgress(e.target);
+});
